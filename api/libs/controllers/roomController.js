9a6610ce8ccc26e983e6be1b6ab70ef6b3679230
thanhnam72 <lythanhnam72@gmail.com>
2021-10-23 18:32:29 +0700

    use console instead log

--- api/libs/controllers/roomController.js
+++ api/libs/controllers/roomController.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const roomService = require('../services/roomService');
 
-router.post('/join', async (req, res) => {
-  const result = await roomService.joinRoom(req.body.username, req.body.roomId);
-  res.json(result);
-});
-
 router.get('/:roomId/messages', async (req, res) => {
   const result = await roomService.getMessagesByRoomId(req.params.roomId);
   res.json(result);
--- api/libs/db/mongoose.js
+++ api/libs/db/mongoose.js
@@ -2,18 +2,17 @@ var mongoose = require('mongoose');
 var libs = process.cwd() + '/libs/';
 var log = require(libs + 'log')(module);
 var config = require(libs + 'config');
-console.log(config.get('mongoose:uri'));
 
 mongoose.connect(config.get('mongoose:uri'), { useNewUrlParser: true });
   
 var db = mongoose.connection;
   
 db.on('error', function (err) {
-  log.error('Connection error:', err.message);
+  console.log('Connection error:', err.message);
 });
 
 db.once('open', function callback () {
-  log.info("Connected to DB!");
+  console.log("Connected to DB!");
 });
 
 module.exports = mongoose;
